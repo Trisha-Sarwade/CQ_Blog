90a65527ec448d33c2428e8b6567fc063b2395b1
Trisha-Sarwade <77583269+Trisha-Sarwade@users.noreply.github.com>
2021-11-02 16:19:47 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-
-	// connect to database
-       // coming soon...
-
-	define ('ROOT_PATH', realpath(dirname('C:\xampp\htdocs\CQ_BLOG')));
-	define('BASE_URL', 'http://localhost/complete-blog-php/');
-?>
